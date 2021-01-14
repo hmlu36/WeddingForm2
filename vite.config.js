60dbf4f3bcf61d0e46503bed438ba37e718a079b
hmlu36 <hmlu36@gmail.com>
2021-01-14 11:28:09 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
+import path from 'path'
+
 // vue.config.js
 module.exports = {
+  alias: {
+    '/@/': path.resolve(__dirname, './src')
+  },
   base: '/WeddingForm2/', //發佈對應路徑
   assetsDir: 'assets',
 };
